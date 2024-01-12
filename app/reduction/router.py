9e333c7da9b425e35c2b5e64f5a8f913b75ea47b
artlermontov2 <artist.bryansk@gmail.com>
2024-01-12 19:52:04 +0300

    create redirect, schemas for url

--- app/reduction/router.py
+++ app/reduction/router.py
@@ -1,5 +1,7 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, Response, HTTPException, status
+from fastapi.responses import RedirectResponse
 from hashids import Hashids
+from app.reduction.schemas import UrlItem
 
 
 router = APIRouter(
@@ -12,15 +14,23 @@ def generate_short_url(url: str):
     token = hashids.encrypt(123)
     return token
 
-def get_long_url():
-    pass
+# Достаём из БД длинный url
+def get_long_url(short_url: str):
+    long_url = 'https://habr.com/ru/companies/spectr/articles/715290/'
+    return long_url
 
 @router.post("/shorten")
-async def shorten(url: str):
-    token = generate_short_url(url)
+async def shorten(url: UrlItem):
+    token = generate_short_url(url.long_url)
     short_url = f'https://test.com/{token}'
-    return {"short_url": short_url, 'long_url': url, 'token': token}
+    return {"short_url": short_url, 'long_url': url.long_url, 'token': token}
 
-@router.get("/{short_url}")
-async def redirect_to_original_url():
-    pass
+@router.get("/")
+async def redirect_to_original_url(short_url: str):
+    long_url = get_long_url(short_url)
+    if long_url is None:
+        raise HTTPException(
+            status_code=status.HTTP_404_NOT_FOUND,
+            detail='Original URL not found'
+        )
+    return RedirectResponse(url=long_url)
