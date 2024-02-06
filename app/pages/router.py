a7bf204d415e5937412c478292a563e51d8d8fd5
artlermontov2 <artist.bryansk@gmail.com>
2024-02-06 22:16:45 +0300

    create view my link

--- app/pages/router.py
+++ app/pages/router.py
@@ -2,6 +2,8 @@ from fastapi import APIRouter, Request, Depends
 from fastapi.templating import Jinja2Templates
 from app.users.router import login, register_user, logout
 from app.reduction.router import shorten
+from app.users.dependencies import get_current_user
+from app.users.router import get_my_all_urls
 
 
 router = APIRouter(
@@ -30,4 +32,11 @@ async def register(request: Request):
     return templates.TemplateResponse(
         name="register.html",
         context={"request": request}
+    )
+
+@router.get("/user_link")
+async def register(request: Request, user_links=Depends(get_my_all_urls)):
+    return templates.TemplateResponse(
+        name="user.html",
+        context={"request": request, "user_links": user_links}
     )
