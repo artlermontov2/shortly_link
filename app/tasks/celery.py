a0b742545ab99dae52ac86b84b889e87010b545d
artem-pc <artbvr@mail.ru>
2024-02-27 20:30:13 +0300

    install celery & create celery tasks

---
+++ app/tasks/celery.py
@@ -0,0 +1,15 @@
+import os
+from celery import Celery
+from dotenv import load_dotenv
+
+
+load_dotenv()
+
+REDIS_HOST = os.getenv('REDIS_HOST')
+REDIS_PORT = os.getenv('REDIS_PORT')
+
+celery = Celery(
+    "tasks",
+    broker=f"redis://{REDIS_HOST}:{REDIS_PORT}",
+    include=["app.tasks.tasks"]
+)
---
+++ app/tasks/tasks.py
@@ -0,0 +1,16 @@
+from datetime import timedelta
+import asyncio
+from app.tasks.celery import celery
+from app.reduction.dao import ReductionDAO
+
+@celery.task
+def periodic_task():
+    loop = asyncio.get_event_loop()
+    loop.run_until_complete(ReductionDAO.delete_after_expire())
+
+celery.conf.beat_schedule = {
+    'add-every-24-hours': {
+        'task': 'app.tasks.tasks.periodic_task',
+        'schedule': timedelta(days=1),
+    },
+}
